6abec82c28b11ac5f1fccfc0202ad1634fada41c
Christophe Villeger <villeger.c@gmail.com>
2020-02-09 03:24:12 +0100

    Init

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("email")
  */
 class User implements UserInterface
 {
@@ -18,6 +21,7 @@ class User implements UserInterface
     private $id;
 
     /**
+     * @Assert\Email()
      * @ORM\Column(length=180, unique=true)
      */
     private $email;
